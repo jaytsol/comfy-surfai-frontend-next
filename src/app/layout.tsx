44ca839b8980bf0fc1c15e061e08dc6f68c5ba99
jaytsol <jaytsol@naver.com>
2025-07-05 16:51:24 +0900

    chore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default async function RootLayout({
     <html lang="ko">
       <body className={`${inter.className} min-h-screen bg-background`}>
         <AuthProvider initialUser={user}>
-          <ClientLayout user={user}>
+          <ClientLayout>
             {children}
           </ClientLayout>
         </AuthProvider>
